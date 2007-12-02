a627250392abc1d226675a1258184d647c138e17
SÃ©bastien Renard <Sebastien.Renard@digitalfox.org>
2007-12-02 22:31:15 +0000

    Forget html ChangeLog Update ChangeLog generator to use SourceForge repository and concatenate old changelog

--- changelog.sh
+++ changelog.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-~/src/svn2cl-0.7/svn2cl.sh --group-by-day file:///home/fox/prog/depot/trunk/pysql/
-~/src/svn2cl-0.7/svn2cl.sh --html --group-by-day file:///home/fox/prog/depot/trunk/pysql/
-svn -m "Changelog update" commit ChangeLog ChangeLog.html
+~/src/svn2cl-0.7/svn2cl.sh --group-by-day https://pysql.svn.sourceforge.net/svnroot/pysql/trunk/pysql
+#~/src/svn2cl-0.7/svn2cl.sh --group-by-day file:///home/fox/prog/depot/trunk/pysql/
+#~/src/svn2cl-0.7/svn2cl.sh --html --group-by-day file:///home/fox/prog/depot/trunk/pysql/
+#~/src/svn2cl-0.7/svn2cl.sh --html --group-by-day https://pysql.svn.sourceforge.net/svnroot/pysql/trunk/pysql
+cat ChangeLog PastChangeLog > ChangeLog.tmp
+mv ChangeLog.tmp ChangeLog
+echo svn -m "Changelog update" commit ChangeLog www/ChangeLog.html
