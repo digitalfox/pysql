97b55535c45e16c242b65d8b6e65619e56aca9d7
SÃ©bastien Renard <Sebastien.Renard@digitalfox.org>
2007-12-12 22:04:31 +0000

    Add simple shell wrapper to help developpers to start pysql from source tree Update setup.py to new package tree

---
+++ pysql.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Sebastien Renard - 2007
+
+# Simple shell wrapper to launch pysql from source package
+# This is intended only for developpers or users that don't want to "install" pysql
+# but just use it from a simple directory
+
+# Tell python where to find packages
+export PYTHONPATH=$(dirname $0)/src
+
+# Start it
+$(dirname $0)/src/bin/pysql
--- setup.py
+++ setup.py
@@ -6,21 +6,22 @@
 
 from distutils.core import setup
 
+# Languages
 langs=["fr"]
 data_files=[]
 for lang in langs:
     data_files.append(["share/locale/%s/LC_MESSAGES/" % lang, 
                        ["locale/%s/LC_MESSAGES/pysql.mo" % lang]])
 
- 
+#Go for setup 
 setup(name="pysql",
       version="0.11",
       description="PySQL is an Oracle enhanced client",
       author="Sébastien Renard and Sébastien Delcros",
       author_email="pysql@digitalfox.org",
       url="http://pysql.sf.net",
-      package_dir={"pysql" : "src"},
+      package_dir={"pysql" : "src/pysql"},
       packages=["pysql"],
-      scripts=["pysql"],
+      scripts=["src/bin/pysql"],
       data_files=data_files
       )
