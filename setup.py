88301ef83b8b30304b2f88c6b6390e3c879d6f1a
SÃ©bastien Renard <Sebastien.Renard@digitalfox.org>
2007-12-15 13:49:59 +0000

    Add post install script for Windows desktop shortcut and start menu entry

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 # Code licensed under GNU GPL V2
 
 from distutils.core import setup
+import sys
 
 # Languages
 langs=["fr"]
@@ -13,15 +14,22 @@ for lang in langs:
     data_files.append(["share/locale/%s/LC_MESSAGES/" % lang, 
                        ["locale/%s/LC_MESSAGES/pysql.mo" % lang]])
 
+# Scripts
+scripts=["src/bin/pysql"]
+
+# Windows post install script
+if "win" in " ".join(sys.argv[1:]):
+    scripts.append("w32_postinst.py")
+
 #Go for setup 
 setup(name="pysql",
       version="0.11",
       description="PySQL is an Oracle enhanced client",
-      author="Sébastien Renard and Sébastien Delcros",
+      author="Sebastien Renard and Sebastien Delcros",
       author_email="pysql@digitalfox.org",
       url="http://pysql.sf.net",
       package_dir={"pysql" : "src/pysql"},
       packages=["pysql"],
-      scripts=["src/bin/pysql"],
+      scripts=scripts,
       data_files=data_files
       )
---
+++ w32_postinst.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Sébastien Renard (sebastien.renard@digitalfox.org)
+# Code licensed under GNU GPL V2
+
+
+""" Post installation script for win32 system
+    
+    This script creat a shortcut for coincoin.py in the desktop and the
+    start menu, and remove them at the uninstallation
+    
+    Thanks to the coin coin projet for the inspiration for this postinstall script
+    
+"""
+
+from os.path import abspath, join
+from os import mkdir
+import sys
+
+# Description string
+desc="PySQL - advanced Oracle client written in Python"
+# Shortcut name
+lnk="pysql.lnk"
+
+# Only do things at install stage, not uninstall
+if sys.argv[1] == "-install":
+    # Get python.exe path
+    py_path = abspath(join(sys.prefix, "python.exe"))
+    
+    # Pysql wrapper path
+    pysql_dir=abspath(join(sys.prefix, "scripts"))
+    pysql_path=join(pysql_dir, "pysql")
+                          
+    #TODO: create a sexy pysql .ico file to be put in share dir
+    
+    
+    # Find desktop    
+    try:
+        desktop_path = get_special_folder_path("CSIDL_COMMON_DESKTOPDIRECTORY")
+    except OSError:
+        desktop_path = get_special_folder_path("CSIDL_DESKTOPDIRECTORY")
+    
+    # Desktop shortcut creation
+    create_shortcut(py_path, # program to launch
+                    desc,
+                    join(desktop_path, lnk),  # shortcut file
+                    pysql_path, # Argument (pythohn script)
+                    pysql_dir, # Current work dir
+                    "" # Ico file (nothing for now)
+                    )
+
+    # Tel install process that we create a file so it can removed it during uninstallation
+    file_created(join(desktop_path, lnk))
+
+    # Start menu shortcut creation
+    try:
+        start_path = get_special_folder_path("CSIDL_COMMON_PROGRAMS")
+    except OSError:
+        start_path = get_special_folder_path("CSIDL_PROGRAMS")
+
+    # Création du dossier dans le menu programme
+    programs_path = join(start_path, "PySQL")
+    try:
+        mkdir(programs_path)
+    except OSError:
+        pass
+    directory_created(programs_path)
+
+    create_shortcut(py_path, # program to launch
+                    desc, 
+                    join(programs_path, lnk),  # Shortcut file
+                    pysql_path, # Argument (python script)
+                    pysql_dir, # Cuurent work dir
+                    "" # Icone
+                    )
+    file_created(join(programs_path, lnk))
+
+    # End of script
+    sys.exit()
