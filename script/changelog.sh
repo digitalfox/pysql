7a5b318cc8a68fa066902525edb7ad7293147d87
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2008-12-14 22:54:56 +0100

    New changelog generation script working with git

--- script/changelog.sh
+++ script/changelog.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)/..
-~/src/svn2cl-0.7/svn2cl.sh --group-by-day https://pysql.svn.sourceforge.net/svnroot/pysql/trunk/pysql
-#~/src/svn2cl-0.7/svn2cl.sh --group-by-day file:///home/fox/prog/depot/trunk/pysql/
-#~/src/svn2cl-0.7/svn2cl.sh --html --group-by-day file:///home/fox/prog/depot/trunk/pysql/
-#~/src/svn2cl-0.7/svn2cl.sh --html --group-by-day https://pysql.svn.sourceforge.net/svnroot/pysql/trunk/pysql
-cat ChangeLog PastChangeLog > ChangeLog.tmp
-mv ChangeLog.tmp ChangeLog
+git --no-pager log > ChangeLog.tmp
+cat ChangeLog.tmp PastChangeLog > ChangeLog
+rm ChangeLog.tmp
