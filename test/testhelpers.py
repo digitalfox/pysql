a237eebb380ec81d190c7c162a9e8a7e7582ddef
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2009-01-04 19:21:50 +0100

    Take care of user locale Disable colors during test to ease string comparison Add an stdout capture mechanism to be able to test pysql shell

--- test/testhelpers.py
+++ test/testhelpers.py
@@ -7,11 +7,62 @@
 
 import gettext
 import sys
+import os
 from os.path import abspath, dirname, join, pardir
+from tempfile import TemporaryFile
 
 def setup():
+    # Set  PYDEVDEBUG to 1 in order to desactivate shell colors
+    os.environ["PYDEVDEBUG"]="1"
+
     # Setup gettext to avoid error with the global _() function
     gettext.install("pysql", "", unicode=1)
 
     # Add pysql module path
     sys.path.append(abspath(join(dirname(__file__), pardir, "src", "pysql")))
+
+    # Set locale
+    # Loads config (first time)
+    from pysqlconf import PysqlConf
+    conf=PysqlConf.getConfig()
+
+    # Sets the locale
+    import pysqlmain
+    pysqlmain.setLocale(conf)
+
+class CapturedStdout:
+    """Capture sys.out output in temp file to allow function result testing
+    Thanks to Catherine Devlin (catherinedevlin.blogspot.com) for the idea"""
+
+    def __init__(self):
+        """Capture stdout"""
+        self.backupStdout=sys.stdout
+        self.tmpFile=TemporaryFile()
+        sys.stdout=self.tmpFile
+
+    def readlines(self, reset=True):
+        """
+        @param reset: reset buffer for next usage (default is True)
+        @return: array of lines captured and reset buffer"""
+        self.tmpFile.seek(0)
+        lines=self.tmpFile.readlines()
+        if reset:
+            self.reset()
+        return [line.strip("\n").strip("\x00") for line in lines]
+
+    def reset(self):
+        """Reset stdout buffer"""
+        self.tmpFile.truncate(0)
+
+    def gotPsyqlException(self):
+        """Look if captured output has a PysqlException
+        @return: True is got exception, else False"""
+        lines=self.readlines()
+        for line in lines:
+            if "Pysql error" in line:
+                return True
+        return False
+
+    def restoreStdout(self):
+        sys.stdout=self.backupStdout
+        self.tmpFile.close()
