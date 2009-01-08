f774ab91e53c52861fe33d63f703b6814daaa44f
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2009-01-08 08:21:47 +0100

    Add unittest for do_disconnect and do_history

--- test/test_src_pysql_pysqlshell.py
+++ test/test_src_pysql_pysqlshell.py
@@ -7,6 +7,7 @@
 """
 
 import unittest
+import os
 import sys
 
 # Common test pysql tools
@@ -93,12 +94,37 @@ class TestConnectedShellCommands(TestShellCommands):
         if self.capturedStdout.gotPsyqlException() and self.lastExceptionOraCode()!="ORA-01031":
             self.fail("Cannot connect as sysdba or bad ORA code")
 
+    def test_do_disconnect(self):
+        self.exeCmd("disconnect")
+        self.failIf(self.capturedStdout.gotPsyqlException())
+        # Reconnect for other tests
+        self.exeCmd("connect %s" % CONNECT_STRING)
+        self.failIf(self.capturedStdout.gotPsyqlException())
+
 
 class TestNotConnectedShellCommands(TestShellCommands):
     """Tests for all commands that do not need an Oracle connection"""
     def test_do_showCompletion(self):
         self.exeCmd("showCompletion")
         self.failIf(self.capturedStdout.gotPsyqlException())
+
+    def test_do_history(self):
+        try:
+            import readline
+            # This will match the next command position
+            position=readline.get_current_history_length()
+        except Exception, e:
+            position=""
+            if os.name=="posix":
+                self.fail("Cannot use readline: %s" % e)
+
+        for cmd in ("history", "history %s" % position):
+            self.exeCmd(cmd)
+            self.failIf(self.capturedStdout.gotPsyqlException())
+
+        for cmd in ("history a", "history -1", "history 10.5", "history 10,5", "history 2 4"):
+            self.exeCmd(cmd)
+            self.failUnless(self.capturedStdout.gotPsyqlException())
 
 
 class TestCompleters(unittest.TestCase):
