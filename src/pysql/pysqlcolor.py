e5336264947e8f523d0e4f44bbff7ded2e6af066
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2010-02-11 19:13:59 +0100

    Disable color when input or output is not a tty

--- src/pysql/pysqlcolor.py
+++ src/pysql/pysqlcolor.py
@@ -8,24 +8,28 @@
 
 # Python imports:
 import os
+import sys
 
-if os.name=='posix' and os.getenv("PYDEVDEBUG", "0")=="0":
-    BOLD    = '\033[01m'
-    RED     = '\033[31m'
-    GREEN   = '\033[32m'
-    ORANGE  = '\033[33m'
-    BLUE    = '\033[34m'
-    PURPLE  = '\033[35m'
-    CYAN    = '\033[36m'
-    GREY    = '\033[37m'
-    RESET   = '\033[0;0m'
+if os.name == 'posix' and \
+   os.getenv("PYDEVDEBUG", "0") == "0" and \
+   sys.stdin.isatty() and \
+   sys.stdout.isatty():
+    BOLD = '\033[01m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    ORANGE = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    CYAN = '\033[36m'
+    GREY = '\033[37m'
+    RESET = '\033[0;0m'
 else:
-    BOLD    = ''
-    RED     = ''
-    GREEN   = ''
-    ORANGE  = ''
-    BLUE    = ''
-    PURPLE  = ''
-    CYAN    = ''
-    GREY    = ''
-    RESET   = ''
+    BOLD = ''
+    RED = ''
+    GREEN = ''
+    ORANGE = ''
+    BLUE = ''
+    PURPLE = ''
+    CYAN = ''
+    GREY = ''
+    RESET = ''
